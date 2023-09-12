b81dbb8763fdf795f28ca72827e64368cbafd1e3
Fatiha Laabidi <fatihalaabidipro31@gmail.com>
2023-09-12 23:51:33 +0200

    threeth

--- js/script.js
+++ js/script.js
@@ -9,10 +9,6 @@ menuIcon.onclick = () => {
 }
 
 
-
-
-
-
 /* scroll sections active link*/
 
 let sections = document.querySelectorAll('section')
@@ -38,7 +34,27 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
+    /* remove toggle icon and navbar click navbar link*/
+
+    // menuIcon.classList.toggle('bx-x');
+    // navbar.classList.toggle('active');
+    
+
 }
+
+ScrollReveal({ 
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ScrollReveal().reveal('.home-img, .tarifs-container, .galerie-box, .contact', {origin:'bottom'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin:'left'});
+
+
 
 
 
