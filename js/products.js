14556dd4616ce09c8a55ebaa5d45937c3588bf86
Fatiha Laabidi <fatihalaabidipro31@gmail.com>
2023-10-07 20:09:40 +0200

    tenth

--- js/products.js
+++ js/products.js
@@ -1,48 +1,18 @@
 fetch('products.json')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                appendData(data);
-            })
-            .catch(function (err) {
-                console.log('error: ' + err);
-            });
-        function appendData(data) {
-            var mainContainer = document.getElementById("myData");
-            for (var i = 0; i < data.length; i++) {
-                var div = document.createElement("div");
-                div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-                mainContainer.appendChild(div);
-            }
-        }
-
-// fetch("products.json")
-// .then(function(response){
-
-//     return response.json();
-// }
-// )
-
-// .then(function(products){
-//     let placeholder = document.querySelector("#data-output")
-
-//     let out ="";
-
-//     for (let product of products){
-    
-//       out +=  "
-
-//       <tr>
-//         <td><img src='${product.image}'> </td>
-//         <td>${product.name}</td>
-//         <td>${product.name}</td>
-//         <td>${product.name}</td>
-//       </tr>
-//       " ;
-
-    
-        
-//     }
-    
-// })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+  function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+        mainContainer.appendChild(div);
+    }
+}
