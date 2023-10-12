c274aa7283b417fcadbc61e83abceb9a1e38c505
Fatiha Laabidi <fatihalaabidipro31@gmail.com>
2023-10-12 21:19:33 +0200

    eighth

--- js/products.js
+++ js/products.js
@@ -1,18 +1,31 @@
-fetch('products.json')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    appendData(data);
-})
-.catch(function (err) {
-    console.log('error: ' + err);
+// mes données et en cas d'erreurs
+
+const getData = async () => {
+    try {
+        const response = await fetch("products.json");
+        if (!response.ok) {
+            throw new Error("Réponse non valide depuis le serveur");
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("404 :", error);
+    }
+};
+
+// on prends les elements html
+getData().then((products) => {
+    const productContainer = document.getElementById("product");
+    const productTemplate = document.querySelector(".card-template");
+
+    products.forEach(product => {
+        const productCard = productTemplate.cloneNode(true);
+        
+        productCard.querySelector(".product-image").src = product.image;
+        productCard.querySelector(".product-title").textContent = product.title;
+        productCard.querySelector(".product-description").textContent = product.description;
+        productCard.querySelector(".product-price").textContent = `Prix : ${product.price}`;
+
+        productContainer.appendChild(productCard);
+    });
 });
-function appendData(data) {
-var mainContainer = document.getElementById("myData");
-for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = data[i].image + ' ' + data[i].title + ' ' + data[i].description;
-    mainContainer.appendChild(div);
-}
-}
